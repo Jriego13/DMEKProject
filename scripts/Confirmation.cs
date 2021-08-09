5fe2996d2c4cdf91fa8a7b810a48cbc69674ec60
herman <hperera1@ufl.edu>
2021-08-09 02:53:18 -0400

    some housekeeping. 3d animation test scene added. documentation updated

--- scripts/Confirmation.cs
+++ scripts/Confirmation.cs
@@ -5,14 +5,14 @@ public class Confirmation : RichTextLabel {
   String currentConfirmation;
 
   public override void _Ready() {
-    currentConfirmation = GetNode("/root/Spatial/MainEye").GetChild(0).GetName();
+	currentConfirmation = GetNode("/root/Spatial/MainEye").GetChild(0).GetName();
   }
 
   public override void _Process(float delta) {
-    SetText("Current Conformation: " + currentConfirmation);
-    currentConfirmation = GetNode("/root/Spatial/MainEye").GetChild(0).GetName();
-    if(currentConfirmation == "Finished") {
-      currentConfirmation = "YUP YOU DID IT. GOOD JOB CLICKING :D";
-    }
+	SetText("Current Conformation: " + currentConfirmation);
+	currentConfirmation = GetNode("/root/Spatial/MainEye").GetChild(0).GetName();
+	if(currentConfirmation == "Finished") {
+	  currentConfirmation = "YUP YOU DID IT. GOOD JOB CLICKING :D";
+	}
   }
 }
