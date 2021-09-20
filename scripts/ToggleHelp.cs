09c8333dc2bcf31610204a1d6ef2ec53f34522ce
Herman <hperera1@ufl.edu>
2021-09-20 17:27:00 -0400

    final touches

--- scripts/ToggleHelp.cs
+++ scripts/ToggleHelp.cs
@@ -4,17 +4,17 @@ using System.Threading;
 
 // text at the front which tells you which confirmation you are on
 public class ToggleHelp : RichTextLabel {
-  Boolean showHelp;
+  bool showHelp;
 
   public override void _Ready() {
 	   showHelp = false;
   }
 
-  public override async void _Process(float delta) {
-    if(Input.IsActionPressed("toggle_help")) {
+  public override void _Process(float delta) {
+    if(Input.IsActionJustPressed("toggle_help")) {
       showHelp = !showHelp;
-      await ToSignal(GetTree().CreateTimer(.2f), "timeout");
     }
+
     if (!showHelp) {
         SetText("Press h to show controls");
     }
