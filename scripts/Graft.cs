f642feee418940ee46401586d928b3cbf2fc4e90
herman <44208112+hperera1@users.noreply.github.com>
2021-09-21 20:09:31 -0400

    Revert "Menus"

--- scripts/Graft.cs
+++ scripts/Graft.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 
 // base graft class that I would like to use for all derivative grafts
-public class Graft : Node2D {
+public class Graft : Node {
   protected List<PackedScene> nextConfirmations = new List<PackedScene>();
   protected Random rng = new Random();
   protected int numTaps;
