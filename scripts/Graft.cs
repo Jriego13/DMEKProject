2b6027152091d31e42a48ed3165e365dfc1771a6
Jack <jwittmayer@ufl.edu>
2021-10-10 16:45:18 -0400

    cleaned up rotation code

--- scripts/Graft.cs
+++ scripts/Graft.cs
@@ -17,7 +17,7 @@ public class Graft : Sprite {
   public override void _Input(InputEvent @event)
     {
         base._Input(@event);
-        if (@event.IsActionPressed("left_mouse")&&
+        if (@event.IsActionPressed("left_mouse") &&
           GetRect().HasPoint(ToLocal(GetViewport().GetMousePosition())))
         {
           RotateFromTap();
@@ -25,7 +25,6 @@ public class Graft : Sprite {
     }
   public override void _Process(float delta)
   {
-    //this.Rotate(0.1f);
     Input.SetMouseMode((Godot.Input.MouseMode)0);
     CheckObjectives();
     if (Math.Abs(rotationalVelocity) > 0)
@@ -40,16 +39,19 @@ public class Graft : Sprite {
   }
   protected void Deaccelerate()
   {
+    const float deacceleration = 0.01f;
     this.Rotate((float)rotationalVelocity);
+
+    // Bring rotationalVelocity gradually to 0.
     if (rotationalVelocity > 0)
     {
-      rotationalVelocity -= .01;
+      rotationalVelocity -= deacceleration;
       if (rotationalVelocity < 0)
         rotationalVelocity = 0;
     }
     else
     {
-      rotationalVelocity += .01;
+      rotationalVelocity += deacceleration;
       if (rotationalVelocity > 0)
         rotationalVelocity = 0;
     }
@@ -58,42 +60,46 @@ public class Graft : Sprite {
   private void RotateFromTap()
   {
     Vector2 mousePos = GetViewport().GetMousePosition();
-    Vector2 graftPos = GlobalPosition;
-    
-    // T = F*r*sinθ, force is constant for now
+
     var axis = GetNode("Axis") as Line2D;
-    var axisSlope = (GlobalTransform.BasisXform(axis.Points[0]).y - GlobalTransform.BasisXform(axis.Points[1]).y)/
-    (GlobalTransform.BasisXform(axis.Points[0]).x - GlobalTransform.BasisXform(axis.Points[1]).x + 0.0001);
-    GD.Print("axis slope", axisSlope);
-    //GD.Print(polygon.Polygon);
-    var minDistance = 10000.0;
+    var axisPoint0 = GlobalTransform.BasisXform(axis.Points[0]);
+    var axisPoint1 = GlobalTransform.BasisXform(axis.Points[1]);
+
+    // 1. Get the slope of the axis line. GlobalTransform.BasisXform makes the point coordinates update after rotation.
+    // m = (y1-y2)/(x1-x2)
+    var axisSlope = (axisPoint0.y - axisPoint1.y)/(axisPoint0.x - axisPoint1.x + 0.0001);// add 0.0001 to prevent div by 0.
+
+    // 2. Find closest point on rotational axis to where the user clicked:
+    var minDistance = 100000.0; //start min distance impossibly large.
     var closestAxisPoint = new Vector2();
-    foreach (Vector2 vertex in axis.Points)
+    foreach (Vector2 point in axis.Points)
     {
-      var distance = ToGlobal(vertex).DistanceTo(mousePos);
-      //GD.Print(distance);
+      var distance = ToGlobal(point).DistanceTo(mousePos);
       if (distance < minDistance)
       {
-
         minDistance = distance;
-        closestAxisPoint = ToGlobal(vertex);
+        closestAxisPoint = ToGlobal(point);
       }
     }
-    GD.Print("mouse pos ", mousePos);
-    const float tapForce = 0.001f;
-    var spriteCenter = GetNode("Center") as Line2D;
-    var centerPoint = ToGlobal(spriteCenter.Points[0]);
-    GD.Print("sprite center ", centerPoint);
+    // 3. Find slope of line going from where the user clicked to center of the graft:
+    var centerLine = GetNode("Center") as Line2D;
+    var centerPoint = ToGlobal(centerLine.Points[0]);
     var slope = (mousePos.y - centerPoint.y)/(mousePos.x - centerPoint.x);
-    var y_intercept = mousePos.y - (slope * mousePos.x);
-    var r = Math.Abs(centerPoint.DistanceTo(closestAxisPoint));
-    GD.Print("slope ", slope);
-    var theta = Math.Atan((slope - axisSlope)/(1+slope * axisSlope));
-    GD.Print("theta", theta);
-    var torque = tapForce * r * Math.Sin(theta);
-    rotationalVelocity = torque;
-    GD.Print(torque);
 
-    //var xTorque = tapForce * Math.Abs(mousePos.x - graftPos.x) * 
+    // 4. Calculate r and θ:
+    var r = Math.Abs(centerPoint.DistanceTo(closestAxisPoint));
+    var theta = Math.Atan((slope - axisSlope)/(1+slope * axisSlope)); // tanθ = (m1-m2)/(1+m1*m2)
+
+    // 5. Calculate torque, T = F*r*sinθ:
+    const float tapForce = 0.001f;
+    var torque = tapForce * r * Math.Sin(theta);
+    rotationalVelocity = torque; // Make rotational velocity the torque because things are complicated enough
+
+    // GD.Print("axis slope ", axisSlope);
+    // GD.Print("mouse pos ", mousePos);
+    // GD.Print("slope of line from mouse to center ", slope);
+    // GD.Print("r ", r);
+    // GD.Print("theta ", theta);
+    // GD.Print("torque ", torque);
   }
 }
