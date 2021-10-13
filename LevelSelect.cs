65d112054c19bac2756ec9bdbe6bd3e7ab56fd0a
Herman <hperera1@ufl.edu>
2021-10-13 16:32:03 -0400

    We have two continue buttons now but the game wouldnt work without a 'play' and a 'continue'

--- LevelSelect.cs
+++ LevelSelect.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-// This class gains all of its functionality from LevelMenu.cs, but is remaining in case 
+// This class gains all of its functionality from LevelMenu.cs, but is remaining in case
 // non-tutorial specific logic is needed in the future.
 public class LevelSelect : LevelMenu
 {
@@ -9,7 +9,7 @@ public class LevelSelect : LevelMenu
     {
         var goBackButton = GetNode("MarginContainer/VBoxContainer2/HBoxContainer2/GoBackButton");
         goBackButton.Connect("pressed", this, "onGoBackButtonPressed");
-        
+
         // Create level select button variables:
         String levelHBoxPath = "MarginContainer/VBoxContainer2/levelHBox";
         var simpleFoldButton = GetNode(levelHBoxPath + "/Levels1and4/Level1/SimpleFold");
@@ -18,5 +18,5 @@ public class LevelSelect : LevelMenu
         var tacoButton = GetNode(levelHBoxPath + "/Levels2and5/Level5/Taco");
         var doubleScrollButton = GetNode(levelHBoxPath + "/Levels3and6/Level3/DoubleScroll");
         var edgeFoldButton = GetNode(levelHBoxPath + "/Levels3and6/Level6/EdgeFold");
-
+    }
 }
