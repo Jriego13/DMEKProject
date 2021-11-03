ffcd04a1940fdd0f8200d1c4cea4090eea7dc254
Herman <hperera1@ufl.edu>
2021-11-03 14:58:38 -0400

    latest changes

--- scripts/Taco.cs
+++ scripts/Taco.cs
@@ -43,20 +43,20 @@ public class Taco : Graft {
     currentCannula.inArea = false;
   }
 
-  public void setUpHitboxes(bool setup) {
-	    tapHitBox = GetNode("TapArea/TapHitbox/TapHitboxColorRect") as ColorRect;
-  	  holdHitBox = GetNode("HoldArea/HoldHitbox/HoldHitboxColorRect") as ColorRect;
-      Color tapHitBoxColor = new Color( 0.98f, 0.5f, 0.45f, .5f );
-      Color holdHitBoxColor = new Color( 0.5f, 1f, 0f, .5f );
-	    tapHitBox.Color = tapHitBoxColor;
-	    holdHitBox.Color = holdHitBoxColor;
-      if (setup) {
-        tapHitBox.SetVisible(true);
-        holdHitBox.SetVisible(true);
-      }
-      else {
-        tapHitBox.SetVisible(false);
-        holdHitBox.SetVisible(false);
-      }
-  }
+  // public void setUpHitboxes(bool setup) {
+	//     tapHitBox = GetNode("TapArea/TapHitbox/TapHitboxColorRect") as ColorRect;
+  // 	  holdHitBox = GetNode("HoldArea/HoldHitbox/HoldHitboxColorRect") as ColorRect;
+  //     Color tapHitBoxColor = new Color( 0.98f, 0.5f, 0.45f, .5f );
+  //     Color holdHitBoxColor = new Color( 0.5f, 1f, 0f, .5f );
+	//     tapHitBox.Color = tapHitBoxColor;
+	//     holdHitBox.Color = holdHitBoxColor;
+  //     if (setup) {
+  //       tapHitBox.SetVisible(true);
+  //       holdHitBox.SetVisible(true);
+  //     }
+  //     else {
+  //       tapHitBox.SetVisible(false);
+  //       holdHitBox.SetVisible(false);
+  //     }
+  // }
 }
